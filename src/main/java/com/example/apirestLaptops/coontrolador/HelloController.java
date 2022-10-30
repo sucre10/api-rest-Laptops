3100bfce7725b4a717b2c291ec13e246c40ac6f6
Milton Soto <miltonsm68@gmail.com>
2022-10-30 18:14:44 -0400

    personalizacion de body

--- src/main/java/com/example/apirestLaptops/coontrolador/HelloController.java
+++ src/main/java/com/example/apirestLaptops/coontrolador/HelloController.java
@@ -36,7 +36,9 @@ public class HelloController {
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
                   </head>
                   <body>
-                    <h1>Hello, world!</h1>
+                    <h1>Hola!</h1>
+                    <h3>Esta es la página prncipal de la aplicacion</h3>
+                    <h5>GRACIAS HEROKU</h5>
                     <a class="btn btn-primary"  href="https://www.google.com>Google</a>
                     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
                   </body>
