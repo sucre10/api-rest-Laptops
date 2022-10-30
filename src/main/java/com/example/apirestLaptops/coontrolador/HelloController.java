1847197b67b635901e66bb9bb97dd931a5c546e1
Milton Soto <miltonsm68@gmail.com>
2022-10-30 18:10:30 -0400

    correccion url app principal

--- src/main/java/com/example/apirestLaptops/coontrolador/HelloController.java
+++ src/main/java/com/example/apirestLaptops/coontrolador/HelloController.java
@@ -18,7 +18,29 @@ public class HelloController {
      */
     @GetMapping("/saludo")
     public String Saludo(){
-        return "Hola esta es la tarea SECIOON 4, 5, 6";
+        return "Hola desde HEROKU";
     }
 
+    //------------------
+    @GetMapping("/")  //inicio de la aplicacion
+    public String entrada(){
+        //return """
+        //       """;
+        return """
+                <!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>Bootstrap demo</title>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+                  </head>
+                  <body>
+                    <h1>Hello, world!</h1>
+                    <a class="btn btn-primary"  href="https://www.google.com>Google</a>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+                  </body>
+                </html>
+                """;
+    }
 }
